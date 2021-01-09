8bbbd19358afb3d707d0cc99408ce830c286d771
IsraelMTZGLZ <Israelmg250598@gmail.com>
2021-01-09 11:33:48 -0600

    agregado

--- application/views/Login/register_view.php
+++ application/views/Login/register_view.php
@@ -1,70 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Login V1</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resources/Login/vendor/bootstrap/css/bootstrap.min.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resources/Login/fonts/font-awesome-4.7.0/css/font-awesome.min.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resources/Login/vendor/animate/animate.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resources/Login/vendor/css-hamburgers/hamburgers.min.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resources/Login/vendor/select2/select2.min.cssotstrap/css/bootstrap.min.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resources/Login/css/util.css');?>">
-	<link rel="stylesheet" type="text/css" href="<?=base_url('resources/Login/css/main.css');?>">
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('resources/Login/vendor/bootstrap/css/bootstrap.min.css'); ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('resources/Login/fonts/font-awesome-4.7.0/css/font-awesome.min.css'); ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('resources/Login/vendor/animate/animate.css'); ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('resources/Login/vendor/css-hamburgers/hamburgers.min.css'); ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('resources/Login/vendor/select2/select2.min.cssotstrap/css/bootstrap.min.css'); ?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?= base_url('resources/Login/css/util.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('resources/Login/css/main.css'); ?>">
 
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body>
 
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="<?=base_url('resources/landingpage/img/core-img/colectivo-removebg-preview.png');?>" alt="IMG">
+					<img src="<?= base_url('resources/landingpage/img/core-img/colectivo-removebg-preview.png'); ?>" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form">
 					<span class="login100-form-title">
 						Crear una cuenta
 					</span>
-          <div class="wrap-input100 validate-input" data-validate = "Valid name is required: Xugua">
-            <input class="input100" type="text" name="name" placeholder="Nombre">
-            <span class="focus-input100"></span>
-            <span class="symbol-input100">
-              <i class="fa fa-user" aria-hidden="true"></i>
-            </span>
-          </div>
-          <div class="wrap-input100 validate-input" data-validate = "Valid apellido is required: apellido">
-            <input class="input100" type="text" name="apellido" placeholder="Apellido(s)">
-            <span class="focus-input100"></span>
-            <span class="symbol-input100">
-              <i class="fa fa-user" aria-hidden="true"></i>
-            </span>
-          </div>
-
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate="Valid name is required: Xugua">
+						<input class="input100" type="text" name="name" placeholder="Nombre">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate="Valid apellido is required: apellido">
+						<input class="input100" type="text" name="apellido" placeholder="Apellido(s)">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Correo">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</span>
 					</div>
-
-
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
 							Registrarte
 						</button>
 					</div>
-
 					<div class="text-center p-t-12">
 						<span class="txt1">
 							Olvidé mi
@@ -73,29 +71,23 @@
 							contraseña
 						</a>
 					</div>
-
-          <div class="text-center p-t-12">
-            <span class="txt3">
-              crear cuenta usando
-            </span>
-
-
-          </div>
-          <div class="text-center p-t-12">
-
-              <a class="" href="Registro/googleRegister" style="text-transform:none">
-                  <div class="left">
-                      <img width="40px" style="margin-top:0px; margin-right:0px" alt="Google sign-in"
-                          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
-                  </div>
-                  <!-- <i class="fa fa-google" style="width:6; height:6;background:red; color:white; border: 20px solid red;
+					<div class="text-center p-t-12">
+						<span class="txt3">
+							crear cuenta usando
+						</span>
+					</div>
+					<div class="text-center p-t-12">
+						<a class="" href="Registro/googleRegister" style="text-transform:none">
+							<div class="left">
+								<img width="40px" style="margin-top:0px; margin-right:0px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+							</div>
+							<!-- <i class="fa fa-google" style="width:6; height:6;background:red; color:white; border: 20px solid red;
       border-radius: 50%;"></i> -->
-              </a>
-          </div>
+						</a>
+					</div>
 					<div class="text-center p-t-70">
-						<a class="txt2" href="<?= ("Login") ? './Login' : 'Login' ;?>">
-              Ya tengo una cuenta
-
+						<a class="txt2" href="<?= ("Login") ? './Login' : 'Login'; ?>">
+							Ya tengo una cuenta
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 					</div>
@@ -107,22 +99,23 @@
 
 
 
-<!--===============================================================================================-->
-	<script src="<?=base_url('resources/Login/vendor/jquery/jquery-3.2.1.min.js');?>"></script>
-<!--===============================================================================================-->
-	<script src="<?=base_url('resources/Login/vendor/bootstrap/js/popper.js');?>"></script>
-	<script src="<?=base_url('resources/Login/vendor/bootstrap/js/bootstrap.min.js');?>"></script>
-<!--===============================================================================================-->
-	<script src="<?=base_url('resources/Login/vendor/select2/select2.min.js');?>"></script>
-<!--===============================================================================================-->
-	<script src="<?=base_url('resources/Login/vendor/tilt/tilt.jquery.min.js');?>"></script>
-	<script >
+	<!--===============================================================================================-->
+	<script src="<?= base_url('resources/Login/vendor/jquery/jquery-3.2.1.min.js'); ?>"></script>
+	<!--===============================================================================================-->
+	<script src="<?= base_url('resources/Login/vendor/bootstrap/js/popper.js'); ?>"></script>
+	<script src="<?= base_url('resources/Login/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+	<!--===============================================================================================-->
+	<script src="<?= base_url('resources/Login/vendor/select2/select2.min.js'); ?>"></script>
+	<!--===============================================================================================-->
+	<script src="<?= base_url('resources/Login/vendor/tilt/tilt.jquery.min.js'); ?>"></script>
+	<script>
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-<!--===============================================================================================-->
-	<script src="<?=base_url('resources/Login/js/main.js');?>"></script>
+	<!--===============================================================================================-->
+	<script src="<?= base_url('resources/Login/js/main.js'); ?>"></script>
 
 </body>
+
 </html>
